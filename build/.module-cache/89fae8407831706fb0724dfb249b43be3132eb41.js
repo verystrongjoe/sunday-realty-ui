6709ef6fcc594e702db40921fc228fbd886ecf37
verystrongjoe <verystrongjoe@gmail.com>
2016-05-20 23:13:42 +0900

    first commit

--- build/.module-cache/89fae8407831706fb0724dfb249b43be3132eb41.js
+++
@@ -1,7 +0,0 @@
-var React = require('react')
-var DataGrid = require('react-datagrid')
-
-React.render(
-  React.createElement('h1', null, 'Hello, world!'),
-  document.getElementById('example')
-);
