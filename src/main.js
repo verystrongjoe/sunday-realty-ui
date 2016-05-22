b88e177c8cdc922b46dff195068dd9c44bf57b27
verystrongjoe <verystrongjoe@gmail.com>
2016-05-22 19:24:50 +0900

    temporary commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import World from './world.jsx';
-//import DataGrid from './datagrid-zippyui.jsx';
+import DataGrid from './datagrid-zippyui.jsx';
 //import Comment from './comment.jsx';
 //import CommentBox from './commentbox.jsx'; 
 
